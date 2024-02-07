affa83419575c2644984ac70507b321251f484c2
AbtahiHasan <anikidpersonal@gmail.com>
2024-02-07 17:28:54 +0600

    academic semister ui and functionalty done

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Toaster } from "sonner";
 import RootLayout from "./layout/RootLayout";
 import ProtectedRoute from "./private/ProtectedRoute";
 
@@ -6,6 +7,7 @@ function App() {
     <>
       <ProtectedRoute>
         <RootLayout />;
+        <Toaster />
       </ProtectedRoute>
     </>
   );
--- src/pages/admin/academicManagement/CreateAcademicSemester.tsx
+++ src/pages/admin/academicManagement/CreateAcademicSemester.tsx
@@ -6,6 +6,8 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import PHSelect from "../../../components/form/PHSelect";
 import { semesterOptions } from "../../../constants/semester";
 import { monthOptions } from "../../../constants/global";
+import { useAddAcademicSemesterMutation } from "../../../redux/features/admin/academicManagement.api";
+import { toast } from "sonner";
 
 const currentYear = new Date().getFullYear();
 const yearOptions = [0, 1, 2, 3, 4].map((number) => ({
@@ -14,7 +16,9 @@ const yearOptions = [0, 1, 2, 3, 4].map((number) => ({
 }));
 
 const CreateAcademicSemester = () => {
-  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+  const [addAcademicSemester] = useAddAcademicSemesterMutation();
+  const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+    const toastId = toast.loading("Creating...");
     const semesterData = {
       name: semesterOptions[Number(data.name) - 1]?.label,
       year: data.year,
@@ -23,6 +27,16 @@ const CreateAcademicSemester = () => {
       endMonth: data.endMonth,
     };
     console.log({ semesterData });
+    try {
+      const res = (await addAcademicSemester(semesterData)) as any;
+      if (res.error) {
+        toast.error(res.error.data.message, { id: toastId });
+      } else {
+        toast.success("Semester created", { id: toastId });
+      }
+    } catch (error) {
+      toast.error("Something went wrong", { id: toastId });
+    }
   };
   return (
     <Flex justify="center" align="center">
